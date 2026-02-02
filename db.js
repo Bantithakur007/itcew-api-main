c307d69e63b22431f3d6e31cc8470d96ff9880ed
bantithakur007 <bantithakur145314@gmail.com>
2026-02-03 03:57:54 +0530

    add Routes folder

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://itcew:Admin123@cluster0.8fbnwfu.mongodb.net/?appName=Cluster0";
+const url = process.env.MONGODB_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
